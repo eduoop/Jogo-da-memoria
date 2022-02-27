4996abaeb6f049f392e81df9399fa7e4e7c2c93a
eduoop <85969484+eduoop@users.noreply.github.com>
2022-02-27 15:55:01 -0300

    Construindo as Cartas

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 (function(){
+    var images = []
+
+    for(var i = 0; i < 16; i++) {
+        var img = {
+            src: `images/${i}.jpg`,
+            id: i % 8
+        }
+        images.push(img)
+    }
     startGame()
 
     function startGame() {
+        var frontFaces = document.getElementsByClassName('front')
         for(var i = 0; i < 16; i++) {
             var card = document.querySelector(`#card${i}`)
             card.style.left = i % 8 === 0 ? 5 + 'px' : i % 8 * 165 + 5 + 'px'
             card.style.top = i < 8 ? '5px' : '250px'
 
             card.addEventListener('click', flipCard)
+
+            frontFaces[i].style.background = "url('"+ images[i].src +"')"
+            frontFaces[i].setAttribute("id", images[i].id)
+            console.log(frontFaces[i].id)
         }
     }
 
